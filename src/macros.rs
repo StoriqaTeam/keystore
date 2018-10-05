030a721e555657895345d90fa081d261ec571cb4
Алексей Карасев <karasev.alexey@gmail.com>
2018-10-05 12:22:12 +0300

    minor fix

--- src/macros.rs
+++ src/macros.rs
@@ -140,12 +140,16 @@ macro_rules! ectx {
 /// ```
 /// use diesel::sql_types::{Uuid as SqlUuid, VarChar};
 /// use uuid::Uuid;
+/// #[derive(Debug, Serialize, Deserialize, FromSqlRow, AsExpression, Clone)]
+/// #[sql_type = "SqlUuid"]
 /// pub struct UserId(Uuid);
 /// derive_newtype_sql!(user_id, SqlUuid, UserId, UserId);
 /// ```
 ///
 /// ```
 /// use diesel::sql_types::{Uuid as SqlUuid, VarChar};
+/// #[derive(Deserialize, FromSqlRow, AsExpression, Clone)]
+/// #[sql_type = "VarChar"]
 /// pub struct AuthenticationToken(String);
 /// derive_newtype_sql!(authentication_token, VarChar, AuthenticationToken, AuthenticationToken);
 /// ```
